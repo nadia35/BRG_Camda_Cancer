e50ef16c98fced9bd7dfbf02e0a30cdee4594b67
Caleb Lindgren <calebmlindgren@gmail.com>
2019-03-07 11:01:00 -0700

    Update chi_squared.py

--- chi_squared.py
+++ chi_squared.py
@@ -8,10 +8,28 @@ import math
 import seaborn as sns
 
 # Load our mutation and drug dataframes
-mutation = pd.read_csv('mutation_curated_wustl', sep='\t')
+mutation = pd.read_csv('mutation_curated_wustl.gz', sep='\t')
 drug = pd.read_csv('Drugs_Matched_Entries.tsv', sep='\t')
 
 # Rename index column in mutation dataframe from 'sample' to 'SampleID' to match drug
+mutation.rename(columns={'sample': 'SampleID'}, inplace=True)
+
+print(mutation) # temp
+print(drug) # temp
 
 # Get the intersection of the two dataframes on the PatientID
-patient_intersect = mutation.merge(drug, on='SampleID')
+# Right now this returns an empty dataframe, because we need to reformat the SampleIDs to batch between the two tables.
+patient_intersect = mutation.merge(drug, on='SampleID') 
+
+print(patient_intersect) # temp
+
+# all below is temporary
+mutation_samples = mutation['SampleID'].tolist()
+drug_samples = drug['SampleID'].tolist()
+
+print("starting sample compare...")
+for sample in mutation_samples:
+    if sample in drug_samples:
+        print(sample)
+
+print("finished sample compare.")
