b635c4de335da8b41ea79805b0f1e904b217a378
Caleb Lindgren <calebmlindgren@gmail.com>
2019-03-07 11:54:24 -0700

    Update chi_squared.py

--- chi_squared.py
+++ chi_squared.py
@@ -11,21 +11,21 @@ import seaborn as sns
 mutation = pd.read_csv('mutation_curated_wustl.gz', sep='\t')
 drug = pd.read_csv('Drugs_Matched_Entries.tsv', sep='\t')
 
+
+
 # Rename 'sample' column in mutation dataframe to 'SampleID' to match drug dataframe
 mutation.rename(columns={'sample': 'SampleID'}, inplace=True)
 
-# Reformat Sample IDs in mutation dataframe to match drug dataframe format
 
-def reformat_sample_ID(sample_ID): # TODO: is this actually the proper way to reformat these? Do we have some documentation we can check?
-#    new_ID = sample_ID[:5] + sample_ID[6] + sample_ID[5] + sample_ID[7:12]
+
+# Reformat Sample IDs in mutation dataframe to match drug dataframe format. Drop additional '-01' at the end of every ID.
+def reformat_sample_ID(sample_ID): 
     new_ID = sample_ID[:12]
     return new_ID
 
-print(mutation['SampleID'])
-
 mutation['SampleID'] = mutation['SampleID'].apply(reformat_sample_ID)
 
-print(mutation['SampleID'])
+
 
 # Get the intersection of the two dataframes on the PatientID
 patient_intersect = mutation.merge(drug, on='SampleID') 
