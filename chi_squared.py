bfe8467ccba390979964a392f5fc08fa864e3100
Caleb Lindgren <calebmlindgren@gmail.com>
2019-03-07 11:53:02 -0700

    Update chi_squared.py
    Reformat SampleIDs and merge mutation and drug dataframes

--- chi_squared.py
+++ chi_squared.py
@@ -11,25 +11,23 @@ import seaborn as sns
 mutation = pd.read_csv('mutation_curated_wustl.gz', sep='\t')
 drug = pd.read_csv('Drugs_Matched_Entries.tsv', sep='\t')
 
-# Rename index column in mutation dataframe from 'sample' to 'SampleID' to match drug
+# Rename 'sample' column in mutation dataframe to 'SampleID' to match drug dataframe
 mutation.rename(columns={'sample': 'SampleID'}, inplace=True)
 
-print(mutation) # temp
-print(drug) # temp
+# Reformat Sample IDs in mutation dataframe to match drug dataframe format
+
+def reformat_sample_ID(sample_ID): # TODO: is this actually the proper way to reformat these? Do we have some documentation we can check?
+#    new_ID = sample_ID[:5] + sample_ID[6] + sample_ID[5] + sample_ID[7:12]
+    new_ID = sample_ID[:12]
+    return new_ID
+
+print(mutation['SampleID'])
+
+mutation['SampleID'] = mutation['SampleID'].apply(reformat_sample_ID)
+
+print(mutation['SampleID'])
 
 # Get the intersection of the two dataframes on the PatientID
-# Right now this returns an empty dataframe, because we need to reformat the SampleIDs to batch between the two tables.
 patient_intersect = mutation.merge(drug, on='SampleID') 
 
-print(patient_intersect) # temp
-
-# all below is temporary
-mutation_samples = mutation['SampleID'].tolist()
-drug_samples = drug['SampleID'].tolist()
-
-print("starting sample compare...")
-for sample in mutation_samples:
-    if sample in drug_samples:
-        print(sample)
-
-print("finished sample compare.")
+print(patient_intersect)
