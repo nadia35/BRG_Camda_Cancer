6691812635ccfd00f6d8c4ec8e3a9e7f41a9f42c
Caleb Lindgren <calebmlindgren@gmail.com>
2019-04-03 23:40:55 -0600

    Update chi_squared_v2.py

--- chi_squared_v2.py
+++ chi_squared_v2.py
@@ -23,13 +23,33 @@ mutation.rename(columns={"sample": "SampleID"}, inplace=True)
 mutation["SampleID"] = mutation["SampleID"].apply(reformat_sample_ID)
 
 # Create an empty dataframe, with one row for each sample ID, and one column for each gene
-
-# For each sample ID, fill in a bool indicating whether it had a mutation for that gene
-# How do we interpret if it had multiple mutations for one gene?
+mutation_sample_ids = sorted(mutation['SampleID'].drop_duplicates())
+genes = sorted(mutation['gene'].drop_duplicates())
+mutation_bools = pd.DataFrame(index=mutation_sample_ids, columns=genes)
 
 # Create an empty dataframe, with one row for each sample ID, and one column for each drug
+drug_sample_ids = sorted(drug['SampleID'].drop_duplicates())
+drugs = sorted(drug['PreferredDrugName'].drop_duplicates())
+drug_reactions = pd.DataFrame(index=drug_sample_ids, columns=drugs)
+
+print(mutation_bools)
+print(drug_reactions)
+
+# For each sample ID in the mutation_bools dataframe, fill in a bool indicating whether it had at least one mutation for that gene
+for sample_id in mutation_bools.index:
+    for gene in mutation_bools.columns:
+        mutated = False
+        if len(mutation.loc[(mutation['gene'] == gene) & (mutation['SampleID'] == sample_id)]) > 0:
+            mutated = True
+        mutation_bools.at[sample_id, gene] = mutated
+        print(mutated)
 
 # Fill in for each drug either what their response was, if they were given it, or if they weren't given it, a null indicator
+for sample_id in drug_reactions.index:
+    for drug_name in drug_reactions.columns:
+        reactions = drug.loc[(drug['SampleID'] == sample_id) & (drug['PreferredDrugName'] == drug_name)]['Response']
+        if len(reactions) > 0:
+            reaction = reactions.iloc[0] # What should we do when there's more than one reaction?
+            drug_reactions.at[sample_id, drug_name] = reaction 
+            print(reaction)
 
-print(mutation)
-print(drug)
